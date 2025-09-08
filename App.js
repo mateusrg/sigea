422513e605ba626d9dbabb3cb4d9dae7b2c0cc61
Victor Santos dos Reis <victorsantosreis6@gmail.com>
2025-09-08 12:04:09 -0300

    Adicionar suporte ao carregamento de fontes e estilos com múltiplas famílias de fontes; reestruturar a configuração do aplicativo.

--- App.js
+++ App.js
@@ -1,12 +1,130 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import LoginScreen from './src/screens/LoginScreen';
 
-export default function App() {
-  const [userProfile, setUserProfile] = useState(null);
+import { useFonts } from "expo-font";
 
+import {
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black,
+  Inter_100Thin_Italic,
+  Inter_200ExtraLight_Italic,
+  Inter_300Light_Italic,
+  Inter_400Regular_Italic,
+  Inter_500Medium_Italic,
+  Inter_600SemiBold_Italic,
+  Inter_700Bold_Italic,
+  Inter_800ExtraBold_Italic,
+  Inter_900Black_Italic,
+} from "@expo-google-fonts/inter";
+
+import {
+  Poppins_100Thin,
+  Poppins_200ExtraLight,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_900Black,
+  Poppins_100Thin_Italic,
+  Poppins_200ExtraLight_Italic,
+  Poppins_300Light_Italic,
+  Poppins_400Regular_Italic,
+  Poppins_500Medium_Italic,
+  Poppins_600SemiBold_Italic,
+  Poppins_700Bold_Italic,
+  Poppins_800ExtraBold_Italic,
+  Poppins_900Black_Italic,
+} from "@expo-google-fonts/poppins";
+
+import {
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from "@expo-google-fonts/roboto";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    // inter
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black,
+    Inter_100Thin_Italic,
+    Inter_200ExtraLight_Italic,
+    Inter_300Light_Italic,
+    Inter_400Regular_Italic,
+    Inter_500Medium_Italic,
+    Inter_600SemiBold_Italic,
+    Inter_700Bold_Italic,
+    Inter_800ExtraBold_Italic,
+    Inter_900Black_Italic,
+    // poppins
+    Poppins_100Thin,
+    Poppins_200ExtraLight,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_900Black,
+    Poppins_100Thin_Italic,
+    Poppins_200ExtraLight_Italic,
+    Poppins_300Light_Italic,
+    Poppins_400Regular_Italic,
+    Poppins_500Medium_Italic,
+    Poppins_600SemiBold_Italic,
+    Poppins_700Bold_Italic,
+    Poppins_800ExtraBold_Italic,
+    Poppins_900Black_Italic,
+    // roboto
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
+  });
+
+  
+  const [userProfile, setUserProfile] = useState(null);
+  
+  if (!fontsLoaded) {
+    return <ActivityIndicator size="large" style={{ flex: 1 }} />;
+  }
+  
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    blue: "#2B6CB0",
+    yellow: "#F6C05A",
+    offWhite: "#FAFBFC",
+    darkGray: "#2D3748",
+    lightGray: "#E2E8F0",
+    green: '#2F855A',
+    orange: '#DD6B20',
+    red: '#E53E3E',
+    lightBlue: '#3182CE'
+}
